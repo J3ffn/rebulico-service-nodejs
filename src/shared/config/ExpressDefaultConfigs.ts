b76bd3993ca5bc4a6a9d9ac09ce3fe03c00f1ed1
j3ffn <jefferson.mangueira12@gmail.com>
2025-11-27 01:01:59 -0300

    add: Adição de limimte para json e urlendoded

--- src/shared/config/ExpressDefaultConfigs.ts
+++ src/shared/config/ExpressDefaultConfigs.ts
@@ -1,4 +1,8 @@
-import { json } from "express";
+import { json, urlencoded } from "express";
 import cors from "cors";
 
-export const configsExpress = [cors({ origin: "*" }), json()];
+export const configsExpress = [
+  cors({ origin: "*" }),
+  json({ limit: "10mb" }),
+  urlencoded({ limit: "10mb", extended: true }),
+];
